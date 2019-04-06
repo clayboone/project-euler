9eb745db1dc719e4ba8df6554d37cbeac5be87d7
Clay Boone <tener.hades@gmail.com>
2019-04-06 12:57:43 +0100

    add: test case where we don't pass mults

--- src/problem002/test_solution.py
+++ src/problem002/test_solution.py
@@ -10,6 +10,9 @@ class TestFiboSummer(unittest.TestCase):
         self.assertListEqual(FiboSummer.build_sequence(89)[-1:], [89],
                              'If possible, sequence should include `n`')
 
+    def test_calculate_all_numbers_in_sequence(self):
+        self.assertEqual(FiboSummer(100).calculate(), 231)
+
     def test_calculate_example(self):
         self.assertEqual(FiboSummer(100, [2]).calculate(), 44)
 
