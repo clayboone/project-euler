655476a692428ba07f1ad0e69aa4d9abd6fcc8e7
Clay Boone <tener.hades@gmail.com>
2019-04-10 13:44:20 +0100

    refactor: a little cleanup

--- mksolution.py
+++ mksolution.py
@@ -5,67 +5,64 @@ solution."""
 import os
 import sys
 
-
-texts = [
-    # 0th: src/problemXXX/solution.py
-    'class Solution(object):\n'
-    '\n'
-    '    def __init__(self, n: int):\n'
-    '        self.answer = n\n'
-    '\n'
-    '    def calculate(self):\n'
-    '        return self.answer\n',
-
-    # 1st: src/problemXXX/readme.txt
-    'Paste the problem description here.\n',
-
-    # 2nd: tests/test_solutionXXX.py
-    'import unittest\n'
-    'from src.problem{}.solution import Solution\n'
-    '\n'
-    '\n'
-    'class TestSolution(unittest.TestCase):\n'
-    '\n'
-    '    def test_solution(self):\n'
-    '        self.assertEqual(Solution(4).calculate(), 4)\n'
-    '\n'
-    '\n'
-    'if __name__ == "__main__":\n'
-    '    unittest.main()\n'
+dirs = [
+    'src',
+    'src/problem{}',
+    'tests'
 ]
+
+files = {
+    'src/problem{}/solution.py':
+        'class Solution(object):\n'
+        '\n'
+        '    def __init__(self, n: int):\n'
+        '        self.answer = n\n'
+        '\n'
+        '    def calculate(self):\n'
+        '        return self.answer\n',
+
+    'src/problem{}/readme.txt':
+        'Paste the problem description here.\n',
+
+    'tests/test_solution{}.py':
+        'import unittest\n'
+        'from src.problem{}.solution import Solution\n'
+        '\n'
+        '\n'
+        'class TestSolution(unittest.TestCase):\n'
+        '\n'
+        '    def test_solution(self):\n'
+        '        self.assertEqual(Solution(4).calculate(), 4)\n'
+        '\n'
+        '\n'
+        'if __name__ == "__main__":\n'
+        '    unittest.main()\n'
+}
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print(f'Usage: {sys.argv[0]} <problem_number>')
 
-    files = [
-        'src/problem{}/solution.py',
-        'src/problem{}/readme.txt',
-        'tests/test_solution{}.py'
-    ]
-
-    dirs = [
-        'src/problem{}'
-    ]
-
     problem_number = sys.argv[1].zfill(3)
 
     # If any of the files already exist, exit with status 1.
     for file in files:
-        if os.path.isfile(file.format(problem_number)):
-            print('File {} already exists. Bailing out.'.format(
-                file.format(problem_number)
-            ))
+        if os.path.exists(file.format(problem_number)):
+            print(f'File {file.format(problem_number)} already exists.')
             sys.exit(1)
 
-    # Create the directory if necessary
+    # Make any folders if necessary
     for d in dirs:
-        if not os.path.isdir(d.format(problem_number)):
-            os.mkdir(d.format(problem_number))
+        formatted_dir = d.format(problem_number)
+
+        if not os.path.isdir(formatted_dir):
+            os.mkdir(formatted_dir)
 
     # None of the files exist yet. Make them and exit.
-    for i, file in enumerate(files):
-        filename = file.format(problem_number)
-        with open(filename, 'w') as f:
-            f.write(texts[i].format(problem_number))
+    for file, content in files.items():
+        formatted_file = file.format(problem_number)
+        formatted_content = content.format(problem_number)
+
+        with open(formatted_file, 'w') as f:
+            f.write(formatted_content)
