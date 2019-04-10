2cacb52fb837c036f40ff480b1cd48a38809d94a
Clay Boone <tener.hades@gmail.com>
2019-04-10 11:10:06 +0100

    remove/change: remove old algo and change usage interface

--- src/problem005/solution.py
+++ src/problem005/solution.py
@@ -3,14 +3,11 @@ import math
 
 class Solution(object):
 
-    def __init__(self, start: int, stop: int):
-        self.start = start
-        self.stop = stop
-
-    def lcm(self) -> int:
+    @staticmethod
+    def lcm(factor_list: 'int[] or generator') -> int:
         resulting_primes_list = {}
 
-        for i in range(self.start, self.stop + 1):
+        for i in factor_list:
             current_primes_list = {}
 
             for factor in Solution.get_prime_factors(i):
@@ -57,30 +54,3 @@ class Solution(object):
             primes.append(n)
 
         return primes
-
-    def lcm_brute(self) -> int:
-        """Return smallest multiple that can be evenenly divided by all numbers
-        in the range from `start` to `stop`
-        """
-        max_mult = 1
-
-        for i in range(self.start, self.stop + 1):
-            max_mult *= i
-
-        for mult in range(1, max_mult + 1):
-
-            # for each number in the range specified
-            for i in range(self.start, self.stop + 1):
-
-                # if the number does not evenly divide this mult
-                if mult % i != 0:
-
-                    # continue looking for another mult
-                    break
-
-            else:
-                # we didn't break from the loop, so this mult can be divided
-                # evenly by all numbers in range specified
-                return mult
-
-        return max_mult
--- tests/test_solution005.py
+++ tests/test_solution005.py
@@ -1,20 +1,16 @@
 import unittest
-from src.problem005.solution import Solution
+from src.problem005.solution import Solution as lcm
 
 
 class TestSolution(unittest.TestCase):
 
     def test_get_prime_factors(self):
-        self.assertListEqual(Solution.get_prime_factors(8), [2, 2, 2])
-        self.assertListEqual(Solution.get_prime_factors(110), [2, 5, 11])
+        self.assertListEqual(lcm.get_prime_factors(8), [2, 2, 2])
+        self.assertListEqual(lcm.get_prime_factors(110), [2, 5, 11])
 
     def test_lcm(self):
-        self.assertEqual(Solution(1, 10).lcm(), 2520)
-        self.assertEqual(Solution(1, 20).lcm(), 232792560)
-
-    def test_lcm_brute(self):
-        self.assertEqual(Solution(1, 10).lcm_brute(), 2520)
-        # self.assertEqual(Solution(1, 20).lcm_brute(), 232792560)
+        self.assertEqual(lcm.lcm(range(1, 11)), 2520)
+        self.assertEqual(lcm.lcm(range(1, 21)), 232792560)
 
 
 if __name__ == "__main__":
