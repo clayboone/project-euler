c58738b25fa51b5fcfc7e62112c91d395e26165d
Clay Boone <tener.hades@gmail.com>
2019-04-10 11:01:46 +0100

    refactor: faster algorithm for problem #5

--- src/problem005/solution.py
+++ src/problem005/solution.py
@@ -1,8 +1,62 @@
+import math
+
+
 class Solution(object):
 
     def __init__(self, start: int, stop: int):
         self.start = start
         self.stop = stop
+
+    def lcm(self) -> int:
+        resulting_primes_list = {}
+
+        for i in range(self.start, self.stop + 1):
+            current_primes_list = {}
+
+            for factor in Solution.get_prime_factors(i):
+                if factor in current_primes_list:
+                    current_primes_list[factor] += 1
+                else:
+                    current_primes_list[factor] = 1
+
+            for prime, count in current_primes_list.items():
+                if prime in resulting_primes_list:
+                    resulting_primes_list[prime] = max([
+                        resulting_primes_list[prime],
+                        count
+                    ])
+                else:
+                    resulting_primes_list[prime] = count
+
+        ret = 1
+        for prime, count in resulting_primes_list.items():
+            ret *= math.pow(prime, count)
+
+        return int(ret)
+
+    @staticmethod
+    def get_prime_factors(n: int) -> 'int[]':
+        """Return a list of the prime factors of `n`."""
+        primes = []
+
+        # where n is 110, primes are [2, 5, 11]
+        # Strip the number of 2s that divide n
+        while n % 2 == 0:
+            primes.append(2)
+            n >>= 1  # n /= 2
+
+        # n is odd now (55), primes are [5, 11]
+        for i in range(3, int(math.sqrt(n)) + 1, 2):
+            # range(3, 8, 2) = [3, 5, 7]
+            while n % i == 0:
+                primes.append(i)
+                n //= i
+
+        # n (11) is still > 2 and must be prime
+        if n > 2:
+            primes.append(n)
+
+        return primes
 
     def lcm_brute(self) -> int:
         """Return smallest multiple that can be evenenly divided by all numbers
--- tests/test_solution005.py
+++ tests/test_solution005.py
@@ -4,10 +4,16 @@ from src.problem005.solution import Solution
 
 class TestSolution(unittest.TestCase):
 
-    def test_solution(self):
-        self.assertEqual(Solution(1, 10).lcm_brute(), 2520)
+    def test_get_prime_factors(self):
+        self.assertListEqual(Solution.get_prime_factors(8), [2, 2, 2])
+        self.assertListEqual(Solution.get_prime_factors(110), [2, 5, 11])
 
-        # Works, but takes ~319.0 seconds on my laptop
+    def test_lcm(self):
+        self.assertEqual(Solution(1, 10).lcm(), 2520)
+        self.assertEqual(Solution(1, 20).lcm(), 232792560)
+
+    def test_lcm_brute(self):
+        self.assertEqual(Solution(1, 10).lcm_brute(), 2520)
         # self.assertEqual(Solution(1, 20).lcm_brute(), 232792560)
 
 
