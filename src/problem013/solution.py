a960cac21940e0837b50ce91cd4bca7edf034fc1
Clay Boone <tener.hades@gmail.com>
2019-04-17 16:56:08 +0100

    add: #13 -> best solution so far x)

---
+++ src/problem013/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    """Python 3 handles "Big Ints" by default and the sum() builtin method
+    returns the sum of all integers in a list.
+
+    This "solution" is 100% calculated in it's test, and this class is useless.
+    """
+
+    def __init__(self):
+        print('Hello, friends :^)')
+
+        raise ImportWarning('Did you mean to import Solution from problem013?')
