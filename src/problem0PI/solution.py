3438d13517f06db3898bd7642fbcb77ba8ef5493
Clay Boone <tener.hades@gmail.com>
2019-05-01 13:12:14 +0100

    refactor: more cleanup

--- src/problem0PI/solution.py
+++ src/problem0PI/solution.py
@@ -1,12 +1,12 @@
 class Solution(object):
 
     @staticmethod
-    def gregory_leibniz(iterations: 'int') -> 'float: Pi':
-        pi = 0
-        subtract = False  # first operation is to add 4/1 to 0.
+    def gregory_leibniz(iterations: 'int') -> 'float':
+        pi = 0.0
+        adding = True
 
         for i in range(1, iterations * 2, 2):
-            pi = pi - 4/i if subtract else pi + 4/i
-            subtract = not subtract
+            pi = pi + 4/i if adding else pi - 4/i
+            adding = not adding
 
         return pi
