922656d0331d21176111ea86c0d150bcf71cca02
Clay Boone <tener.hades@gmail.com>
2019-05-01 14:51:32 +0100

    add: generator for Nilakantha series
    ...my poor laptop...

--- src/problem0PI/solution.py
+++ src/problem0PI/solution.py
@@ -36,10 +36,26 @@ class Solution(object):
 
         return pi
 
+    @staticmethod
+    def iter_nilakantha() -> 'float':
+        """Calculate Pi using the Nilakantha series (fast convergence)"""
+        getcontext().prec = 1000
+        pi = Decimal(3.0)
+        i = 0
+        adding = True
+
+        while True:
+            i += 2
+            t = (i, i+1, i+2)
+            pi = pi + Decimal(4) / Decimal(mult(t)) if adding \
+                else pi - Decimal(4) / Decimal(mult(t))
+            adding = not adding
+
+            yield pi
+
 
 if __name__ == "__main__":
-    i = 0
 
-    while True:
-        print(str(Solution.nilakantha(i))[:120])
-        i += 1000
+    for index, value in enumerate(Solution.iter_nilakantha()):
+        if index % 1_000_000 == 0:
+            print(f'{int(index/1_000_000)} millionth: {str(value)[:140]}')
