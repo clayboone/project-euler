1c8db90ab48f35679681a50f37ac6ff6b9651cf9
Clay Boone <tener.hades@gmail.com>
2019-05-01 13:51:09 +0100

    add/refactor: test for another algorithm to find pi & cleanup

--- tests/test_solution0PI.py
+++ tests/test_solution0PI.py
@@ -1,12 +1,19 @@
 import unittest
+from math import pi
 from src.problem0PI.solution import Solution
+
+FIRST_FIVE_OF_PI = str(pi)[:7]
 
 
 class TestSolution(unittest.TestCase):
 
-    def test_solution(self):
-        result = str(Solution.gregory_leibniz(500_000))
-        self.assertEqual(result[:7], '3.14159')
+    def test_gegory_leibniz(self):
+        result = str(Solution.gregory_leibniz(400_000))[:7]
+        self.assertEqual(result, FIRST_FIVE_OF_PI)
+
+    def test_nilakantha(self):
+        result = str(Solution.nilakantha(40))[:7]
+        self.assertEqual(result, FIRST_FIVE_OF_PI)
 
 
 if __name__ == "__main__":
